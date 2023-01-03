764616fb8c043fc0ce6958cee2405570dae4e226
focuskey <focuskey@gmail.com>
2023-01-04 08:23:21 +1000

    the first meassage for the library module

---
+++ library_app/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': "Manage Library catalog and book lending.",
+
+    'description': "The descripton of this Module. explation",
+
+    'author': "William WEI",
+    'website': "http://www.carpetcall.com.au",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Services/Library',
+    'version': '15.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'application': True,
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/library_menu.xml',
+
+    ],
+    # # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+}
